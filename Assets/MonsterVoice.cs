914e52d2150804eb37e024cb1ddbf034507b4d39
AkarisuMD <AkarisuMD@gmail.com>
2023-06-15 11:52:27 +0200

    des choses

--- Assets/MonsterVoice.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MonsterVoice : Singleton<MonsterVoice>
-{
-    public GameObject positionalChannel;
-}
